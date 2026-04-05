ed105865a2ef83d0e764af1023538510d0043797
ankitabaddoli4 <ankitabaddoli4@gmail.com>
2026-04-05 19:59:33 +0530

    Initialize Flask app with create_app function
    Added a create_app function to initialize the Flask application and register the main blueprint.

--- app/_init_.py
+++ app/_init_.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+def create_app():
+    app = Flask(__name__)
+
+    # 🔗 REGISTER BLUEPRINT
+    from app.main.routes import bp
+    app.register_blueprint(bp)
+
+    return app
